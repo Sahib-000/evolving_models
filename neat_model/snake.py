ddf186f01289d721a20b74d1063831f075c54aba
Sahib <sahibdeepsinghboparai053@gmail.com>
2026-06-12 16:19:31 -0400

    snake example

--- neat_model/snake.py
+++ neat_model/snake.py
@@ -265,3 +265,6 @@ if __name__ == "__main__":
     local_dir = os.path.dirname(__file__)
     config_path = os.path.join(local_dir, "config-snake.txt")
     run(config_path)
+
+
+## Try recurrent model next time
